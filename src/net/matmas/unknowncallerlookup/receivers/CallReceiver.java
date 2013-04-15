c00f6f1a790415547c9eff7c3d0346a131f4518d
Martin Riesz <riesz.martin@gmail.com>
2013-04-15 18:12:51 +0200

    improved compatibility (of getting the incomingNumber)

--- src/net/matmas/unknowncallerlookup/receivers/CallReceiver.java
+++ src/net/matmas/unknowncallerlookup/receivers/CallReceiver.java
@@ -13,37 +13,48 @@ import android.net.Uri;
 import android.os.Handler;
 import android.provider.BaseColumns;
 import android.provider.ContactsContract;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 public class CallReceiver extends BroadcastReceiver {
 
-	private static String TAG = CallReceiver.class.toString();
+//	private static String TAG = CallReceiver.class.toString();
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		final String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);      
 		final String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 		if (TelephonyManager.EXTRA_STATE_RINGING.equals(state) && incomingNumber != null) {       		
-			Log.d(TAG, incomingNumber);
-			if ( !doesContactExist(incomingNumber)) {
-				new Handler().postDelayed(new Runnable() {
-					@SuppressWarnings("deprecation")
-					public void run() {
-						String URL = "https://www.google.com/search?q=" + URLEncoder.encode(incomingNumber);
-						String suffix = MainActivity.loadSearchSuffix();
-						if ( !suffix.equals("")) {
-							URL += URLEncoder.encode(" " + suffix);
-						}
-						Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
-						browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // because we are not calling it from an activity
-//						browserIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//						browserIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-//						browserIntent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-						App.getContext().startActivity(browserIntent);
+			onRinging(incomingNumber);
+		}
+		else {
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			telephonyManager.listen(new PhoneStateListener() {
+	            public void onCallStateChanged(int state, String incomingNumber) {
+	                super.onCallStateChanged(state, incomingNumber);
+	                if (incomingNumber != null && state == TelephonyManager.CALL_STATE_RINGING) {
+	                	onRinging(incomingNumber);
+	                }
+	            }
+	        }, PhoneStateListener.LISTEN_CALL_STATE);
+		}
+	}
+	
+	private void onRinging(final String incomingNumber) {
+    	if ( !doesContactExist(incomingNumber)) {
+			new Handler().postDelayed(new Runnable() {
+				@SuppressWarnings("deprecation")
+				public void run() {
+					String URL = "https://www.google.com/search?q=" + URLEncoder.encode(incomingNumber);
+					String suffix = MainActivity.loadSearchSuffix();
+					if ( !suffix.equals("")) {
+						URL += URLEncoder.encode(" " + suffix);
 					}
-			 	}, 2000);
-			}
+					Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
+					browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // because we are not calling it from an activity
+					App.getContext().startActivity(browserIntent);
+				}
+		 	}, 1000);
 		}
 	}
 	
